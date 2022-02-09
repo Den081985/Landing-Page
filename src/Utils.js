ab887f15c3522486f7a15b4b77973c2e7298a4e4
Den081985 <pishnenkod@gmail.com>
2022-02-09 19:40:05 +0300

    добавлены функции  openQuestionsModal,activateModal,activatePostAuthModal

--- src/Utils.js
+++ src/Utils.js
@@ -1,4 +1,51 @@
-//фуекция валидации входных данных
+import { getFromStorage } from "./Question";
+import { renderModal } from "./Question";
+
+//функция валидации входных данных
 export function formValid(value) {
   return value.length >= 5;
+}
+
+//функция рендеринга модального окна вопросов
+export function openQuestionsModal() {
+  const elements = getFromStorage();
+
+  const html = elements.length
+    ? elements.map(renderModal).join("")
+    : `<div class="mui--text-headline modal-none">ВОПРОСОВ НЕТ</div>`;
+  const modalEl = document.createElement("div");
+  modalEl.className = "modal";
+  modalEl.innerHTML = html;
+
+  // show modal
+  mui.overlay("on", modalEl);
+}
+
+//функция рендеринга окна авторизации
+export function activateModal(title, content) {
+  let modal = document.createElement("div");
+  modal.classList.add("auth-modal");
+
+  const html = `
+    <div class="auth-title">${title}</div>
+    <div class="auth-content">${content}</div>
+  
+  `;
+
+  modal.innerHTML = html;
+  mui.overlay("on", modal);
+}
+//функция рендеринга окна поставторизации
+export function activatePostAuthModal(localId) {
+  let modal = document.createElement("div");
+  modal.classList.add("auth-modal");
+
+  const html = `
+    <div class="auth-title">ВЫ ЗАРЕГИСТРИРОВАНЫ</div>
+    <div class="auth-data">Ваш ID пользователя: ${localId}</div>
+  
+  `;
+
+  modal.innerHTML = html;
+  mui.overlay("on", modal);
 }
