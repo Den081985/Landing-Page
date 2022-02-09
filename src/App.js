bfcd21c9e5757b0d63c5ca820296ac06f3358bf2
Den081985 <pishnenkod@gmail.com>
2022-02-09 19:36:30 +0300

    добавлены функции renderAuthModal, renderEnterModal, authHandler

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import "./styles.css";
-import { formValid } from "./Utils";
+import {
+  activateModal,
+  activatePostAuthModal,
+  formValid,
+  openQuestionsModal,
+} from "./Utils";
 import { Question } from "./Question";
+import { authEmailAndPassword, openAuthEnter, openAuthModal } from "./Auth";
 
 const form = document.getElementById("page-form");
 const theme = form.querySelector("#theme-input");
 const input = form.querySelector("#question-input");
 const submit = form.querySelector("#submit");
-// const modalBtn = document.getElementById("modal-btn");
+const modalBtn = document.getElementById("modal-btn");
+const authBtn = document.getElementById("auth-btn");
+const enterBtn = document.getElementById("enter-btn");
 
 form.addEventListener("submit", submitHandler);
+
+authBtn.addEventListener("click", renderAuthModal);
+
+enterBtn.addEventListener("click", enterHandler);
 
 input.addEventListener("input", () => {
   submit.disabled = !formValid(input.value);
 });
+
+modalBtn.addEventListener("click", openQuestionsModal);
 //функция отправления данных формы
 function submitHandler(event) {
   event.preventDefault();
@@ -34,3 +48,35 @@ function submitHandler(event) {
     });
   }
 }
+
+//функция рендеринга модального окна авторизации
+function renderAuthModal() {
+  activateModal("АВТОРИЗАЦИЯ", openAuthModal());
+  document.getElementById("auth-form").addEventListener("submit", authHandler);
+}
+
+//функция рендеринга модального окна входа
+function renderEnterModal() {
+  activateModal("ВОЙТИ", openAuthEnter());
+  document
+    .querySelector("#enter-form")
+    .addEventListener("submit", enterHandler);
+}
+
+// функция для работы с данными авторизации
+function authHandler(event) {
+  event.preventDefault();
+
+  const email = event.target.querySelector("#email").value;
+  const password = event.target.querySelector("#password").value;
+  const btn = event.target.querySelector("button");
+
+  btn.disabled = true;
+
+  authEmailAndPassword(email, password)
+    .then(activatePostAuthModal)
+    .then(() => (btn.disabled = false));
+}
+
+// функция для работы с данными входа
+function enterHandler() {}
